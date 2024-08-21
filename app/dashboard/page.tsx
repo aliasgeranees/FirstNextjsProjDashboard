55bc42b9695be7f30ff43394c49900b0d465a477
aliasgeranees <anees.aliasger@gmail.com>
2024-08-21 21:39:06 +0530

    Initial Commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Page() {
+    return <div className="flex justify-center">
+        <input className="mt-1 block w-1/4   px-3 py-2 bg-white border border-slate-700 rounded-md text-sm shadow-sm placeholder-slate-400"
+            placeholder="Enter your key here" type="text" name="myKey" />
+    </div>  
+    
+}
